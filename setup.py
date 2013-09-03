61e420be6844938136c13b2a9515e1c0f30ccc23
Tomaz Muraus <tomaz@tomaz.me>
2013-09-03 18:13:31 +0200

    Dissallow accidental publish.

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
+import sys
 from setuptools import setup
+
+
+def forbid_publish():
+    argv = sys.argv
+    blacklist = ['register', 'upload']
+
+    for command in blacklist:
+        if command in argv:
+            values = {'command': command}
+            raise RuntimeError('Command "%(command)s" has been blacklisted' %
+                               values)
+
+forbid_publish()
 
 setup(
     name='email-parser',
